6a9a0835cc568bc02121e0fb0717d41f93df9416
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-21 07:30:39 +0300

    update ✅

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 const app = express();
 
 /*--------- Setting up morgan middleware ---------*/
+
 app.use(morgan('dev'));
 
 /*--------- Setting up cors middleware ---------*/
+
 app.use(cors());
 
 const db = require('./config/db');
@@ -29,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /*--------- Setting up routes ---------*/
+
 const bookRouter = require('./api/routes/bookRouter');
 app.use('/api/v1/books', bookRouter);
 
