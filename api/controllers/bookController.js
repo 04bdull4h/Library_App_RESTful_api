11075822c96891edeea11540a08ad3e9b4f02672
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-21 04:21:59 +0300

    Setting up bookController & bookRouter ✅

---
+++ api/controllers/bookController.js
@@ -0,0 +1,15 @@
+const { Book } = require('../models');
+
+const createBook = async () => {};
+const fetchAllBooks = async () => {};
+const fetchedBookById = async () => {};
+const updateBookById = async () => {};
+const deleteBookById = async () => {};
+
+module.exports = {
+  createBook,
+  fetchAllBooks,
+  fetchedBookById,
+  updateBookById,
+  deleteBookById,
+};
---
+++ api/models/Book.js
@@ -0,0 +1,33 @@
+module.exports = (db, DataType) => {
+  return db.define('books', {
+    id: {
+      type: type.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    isbn: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    publisher: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataType.FLOAT,
+      allowNull: false,
+    },
+  });
+};
