b24a068a09099c3eb2f77610e3d927d39ddcda09
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-22 04:12:35 +0300

    update userRouter docs ✅

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+/**
+ *@doc api/v1/users/ endpoints
+ *@controller userController
+ */
+
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/', userController.fetchAllUsers);
