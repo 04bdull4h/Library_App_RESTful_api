d76d1e1d2401e639b394d949d25e4fd3a30e86c7
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-22 05:12:50 +0300

    fix some bugs ✅

--- api/middlewares/checkTokenAuth.js
+++ api/middlewares/checkTokenAuth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const secretKey = process.env.SECRET_KEY || 'hello';
 
-const checkAuth = (req, res, next) => {
+const checkTokenAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, secretKey);
@@ -17,5 +17,5 @@ const checkAuth = (req, res, next) => {
 };
 
 module.exports = {
-  checkAuth,
+  checkTokenAuth,
 };
