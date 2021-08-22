db8b8e1547fffe6da0badf6358e67799b306e458
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-22 11:22:18 +0300

    update app ✅

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const hpp = require('hpp');
 const app = express();
 
 /*--------- Setting up app config ---------*/
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
@@ -21,6 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
+/*--------- Setting MySQL connection ---------*/
 const db = require('./config/db');
 const dbConnection = async () => {
   try {
@@ -51,18 +51,15 @@ app.use(hpp());
 /*--------- Setting up helmet middlewares ---------*/
 app.use(helmet());
 
-/*--------- Setting up winston middleware ---------*/
-
-/*--------- Setting up routes ---------*/
-
+/*--------- Requiring routes ---------*/
 const bookRouter = require('./api/routes/bookRouter');
 const authorRouter = require('./api/routes/authorRouter');
 const userRouter = require('./api/routes/userRouter');
 
+/*--------- Setting up routes ---------*/
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/authors', authorRouter);
 app.use('/api/v1/users', userRouter);
 
 /*--------- Exporting express app for the server ---------*/
-
 module.exports = app;
