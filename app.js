b128e52f8f7653e7d64f5d83b1a2042136f07dc4
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-22 05:21:13 +0300

    Setting up app config ✅

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
+/*--------- Setting up app config ---------*/
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 /*--------- Setting up morgan middleware ---------*/
 
 app.use(morgan('dev'));
