53ec936f2c29a8077b995ac1a5d9a687dda92684
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-23 21:24:34 +0300

    remove sequelize validators 💻

--- api/models/Author.js
+++ api/models/Author.js
@@ -7,57 +7,22 @@ module.exports = (sequelize, DataTypes) => {
     },
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The first name is required',
-        },
-        len: {
-          args: [2, 55],
-          msg: 'First name should between 2 and 55 characters long',
-        },
-      },
     },
     lastName: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The last name is required',
-        },
-        len: {
-          args: [2, 55],
-          msg: 'Last name should between 2 and 55 characters long',
-        },
-      },
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: {
         args: true,
         msg: 'Email already exists in the database',
       },
-      validate: {
-        notNull: {
-          msg: 'The email is required',
-        },
-        isEmail: {
-          msg: 'You should provide a valid email',
-        },
-      },
     },
     phoneNumber: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: {
         args: true,
         msg: 'Phone number already exists in the database',
-      },
-      validate: {
-        notNull: {
-          msg: 'The phone number is required',
-        },
       },
     },
   });
--- api/models/Book.js
+++ api/models/Book.js
@@ -4,98 +4,31 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      autoIncrement: false,
     },
     title: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The title is required',
-        },
-        len: {
-          args: [3, 30],
-          msg: 'The title should be between 3 and 30 characters long',
-        },
-      },
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The description is required',
-        },
-        len: {
-          args: [30, 300],
-          msg: 'The description should be between 30 and 300 characters long',
-        },
-      },
     },
     author: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The author is required',
-        },
-        len: {
-          args: [4, 64],
-          msg: "The author's name should be between 4 and 64 characters long",
-        },
-      },
     },
     isbn: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: {
         args: true,
         msg: 'ISBN already exists in the database',
       },
-      validate: {
-        notNull: {
-          msg: 'The ISBN is required',
-        },
-        len: {
-          args: [10, 13],
-          msg: 'The ISBN should be between 10 and 13 characters long',
-        },
-      },
     },
     publisher: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The publisher is required',
-        },
-        len: {
-          args: [4, 64],
-          msg: "The publisher's name should be between 4 and 64 characters long",
-        },
-      },
     },
     price: {
       type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The price is required',
-        },
-        isFloat: {
-          msg: 'The price should be float',
-        },
-      },
     },
     status: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'The status is required',
-        },
-      },
     },
   });
   return Book;
