2a5860bf9b2f2c74044f0a3b627c0ea3090fc6af
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-24 00:21:55 +0300

    update login validation 💻

--- api/middlewares/bodyValidatorMiddleware.js
+++ api/middlewares/bodyValidatorMiddleware.js
@@ -1,5 +1,4 @@
 const { check } = require('express-validator');
-const { Book } = require('../models');
 
 const bodyValidatorMiddleware = (method) => {
   switch (method) {
@@ -48,7 +47,7 @@ const bodyValidatorMiddleware = (method) => {
           .withMessage('AuthorId must be a UUID'),
       ];
     }
-    case 'updateBook': {
+    case 'updateBookById': {
       return [
         check('title')
           .optional()
@@ -140,6 +139,53 @@ const bodyValidatorMiddleware = (method) => {
         check('password')
           .exists()
           .withMessage('password is required')
+          .isLength({ min: 6, max: 1024 })
+          .withMessage(
+            'Password length should be between 6 and 1024 characters long'
+          )
+          .matches(
+            /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()+=-\?;,./{}|\":<>\[\]\\\' ~_]).{6,}/
+          )
+          .withMessage(
+            `Password must contain a minimum of 1 lower case letter [a-z], a minimum of 1 upper case letter [A-Z], a minimum of 1 numeric character [0-9], and a minimum of 1 special character: ~\`!@#$%^&*()-_+={}[]|\;:\"<>,./?`
+          ),
+      ];
+    }
+    case 'login': {
+      return [
+        check('email')
+          .exists()
+          .withMessage('email is required')
+          .isString()
+          .withMessage('email must be a string')
+          .isEmail()
+          .withMessage('Please provide a valid email'),
+        check('password').exists().withMessage('password is required'),
+      ];
+    }
+    case 'updateUserById': {
+      return [
+        check('firstName')
+          .optional()
+          .isString()
+          .withMessage('first name must be a string'),
+        check('lastName')
+          .optional()
+          .isString()
+          .withMessage('last name must be a string'),
+        check('username')
+          .optional()
+          .isString()
+          .withMessage('username must be a string')
+          .isLength({ min: 4, max: 16 })
+          .isAlphanumeric()
+          .withMessage('Username must contains only letters and numbers'),
+        check('email')
+          .optional()
+          .isString()
+          .withMessage('email must be a string'),
+        check('password')
+          .optional()
           .isLength({ min: 6, max: 1024 })
           .withMessage(
             'Password length should be between 6 and 1024 characters long'
