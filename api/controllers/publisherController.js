2980e1859ef6b7f82146151136a38582f3b178e1
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-24 03:04:40 +0300

    implement publisher CRUD ops.

--- api/controllers/publisherController.js
+++ api/controllers/publisherController.js
@@ -1,4 +1,4 @@
-const { Publisher } = require('../models');
+const { Publisher, Book } = require('../models');
 const { validationResult } = require('express-validator');
 const {
   okLogger,
@@ -7,9 +7,78 @@ const {
   forbiddenLogger,
   notFoundLogger,
 } = require('../utils/loggerMethods');
-const fetchAllPublishers = async (req, res, next) => {};
-const fetchPublisherById = async (req, res, next) => {};
-const fetchAllBooksByPublisherId = async (req, res, next) => {};
+
+const fetchAllPublishers = async (req, res, next) => {
+  try {
+    const publishers = await Publisher.findAll();
+    if (!publishers) {
+      return res.status(404).json({
+        success: false,
+        message: `There is no publishers in the database right now not`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: 'Publishers fetched successfully',
+      data: publishers,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const fetchPublisherById = async (req, res, next) => {
+  try {
+    const publisherId = req.params.id;
+    const publisher = await Publisher.findByPk(publisherId);
+    if (!publisher) {
+      notFoundLogger(req);
+      return res.status(404).json({
+        success: false,
+        message: `Publisher with id ${publisherId} not found in the database`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: `Publisher with id ${publisherId} fetched successfully`,
+      data: publisher,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const fetchAllBooksByPublisherId = async (req, res, next) => {
+  try {
+    const publisherId = req.params.id;
+    const publisher = await Publisher.findByPk(publisherId);
+    if (!publisher) {
+      notFoundLogger(req);
+      return res.status(404).json({
+        success: false,
+        message: `Publisher with id ${publisherId} not found in the database`,
+        data: {},
+      });
+    }
+    const publishers = await Publisher.findAll({
+      where: { id: publisherId },
+      include: [Book],
+    });
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: `Books related to publisher with id ${publisherId} fetched successfully`,
+      data: publishers,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 const createPublisher = async (req, res, next) => {
   try {
     const errors = validationResult(req);
@@ -34,15 +103,82 @@ const createPublisher = async (req, res, next) => {
     createdLogger(req);
     res.status(201).json({
       success: true,
-      message: 'Book created successfully',
+      message: 'Publisher created successfully',
       data: createdPublisher,
     });
   } catch (err) {
     next(err);
   }
 };
-const updatePublisherById = async (req, res, next) => {};
-const deletePublisherById = async (req, res, next) => {};
+const updatePublisherById = async (req, res, next) => {
+  try {
+    const publisherId = req.params.id;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      badRequestLogger(req);
+      return res.status(400).json({
+        success: false,
+        msg: 'Validation errors',
+        errors: errors.array(),
+      });
+    }
+    const body = {
+      name: req.body.name,
+      address: req.body.address,
+      city: req.body.city,
+      country: req.body.country,
+      specialization: req.body.specialization,
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+    };
+    const publisher = await Publisher.findByPk(publisherId);
+    if (!publisher) {
+      notFoundLogger(req);
+      return res.status(404).json({
+        success: false,
+        message: `Publisher with id ${publisherId} not found in the database`,
+        data: {},
+      });
+    }
+    await Publisher.update(body, {
+      where: {
+        id: publisherId,
+      },
+    });
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: `Publisher with id ${publisherId} updated successfully`,
+      data: body,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+const deletePublisherById = async (req, res, next) => {
+  try {
+    const publisherId = req.params.id;
+    const deletedPublisher = await Publisher.destroy({
+      where: { id: publisherId },
+    });
+    if (!deletedPublisher) {
+      notFoundLogger(req);
+      return res.status(404).json({
+        success: false,
+        message: `Publisher with id ${publisherId} not found in the database`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: `Publisher with id ${publisherId} deleted successfully`,
+      data: {},
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   fetchAllPublishers,
