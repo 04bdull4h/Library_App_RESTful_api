ffce12e077f89b4b0cede88b1c9802e509f7cb25
04bdull4h <s201830040@kfupm.edu.sa>
2021-08-28 23:31:00 +0300

    update borrowedBookController

--- api/controllers/borrowedBookController.js
+++ api/controllers/borrowedBookController.js
@@ -8,55 +8,6 @@ const {
 } = require('../utils/loggerMethods');
 
 /**
- * @description   To create borrowed book
- * @route         POST => api/v1/borrowed-books/
- * @access        Private
- */
-
-const createBorrowedBook = async (req, res, next) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      badRequestLogger(req);
-      return res.status(400).json({
-        success: false,
-        msg: 'Validation errors',
-        errors: errors.array(),
-      });
-    }
-    const body = {
-      title: req.body.title,
-      description: req.body.description,
-      author: req.body.author,
-      isbn: req.body.isbn,
-      publisher: req.body.publisher,
-      status: req.body.status,
-      issueDate: req.body.issueDate,
-      dueDate: req.body.dueDate,
-      BorrowerId: req.body.BorrowerId,
-    };
-    const isBorrowerExists = await Borrower.findByPk(body.BorrowerId);
-    if (!isBorrowerExists) {
-      notFoundLogger(req);
-      return res.status(404).json({
-        success: false,
-        message: `Borrower with id ${body.BorrowerId} not found in the database`,
-        data: {},
-      });
-    }
-    const createdBook = await BorrowedBook.create(body);
-    createdLogger(req);
-    res.status(201).json({
-      success: true,
-      message: `Borrowed Book related to the borrower with id ${body.BorrowerId} created successfully`,
-      data: createdBook,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-/**
  * @description   To fetch all borrowed books
  * @route         GET => api/v1/borrowed-books/
  * @access        Public
@@ -113,26 +64,48 @@ const fetchAllBorrowedBookById = async (req, res, next) => {
 };
 
 /**
- * @description   To fetch all borrowed books
+ * @description   To create borrowed book
  * @route         POST => api/v1/borrowed-books/
- * @access        Public
+ * @access        Private
  */
 
-const fetchAllBorrowedBooks = async (req, res, next) => {
+const createBorrowedBook = async (req, res, next) => {
   try {
-    const borrowedBooks = await BorrowedBook.findAll();
-    if (!borrowedBooks) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      badRequestLogger(req);
+      return res.status(400).json({
+        success: false,
+        msg: 'Validation errors',
+        errors: errors.array(),
+      });
+    }
+    const body = {
+      title: req.body.title,
+      description: req.body.description,
+      author: req.body.author,
+      isbn: req.body.isbn,
+      publisher: req.body.publisher,
+      status: req.body.status,
+      issueDate: req.body.issueDate,
+      dueDate: req.body.dueDate,
+      BorrowerId: req.body.BorrowerId,
+    };
+    const isBorrowerExists = await Borrower.findByPk(body.BorrowerId);
+    if (!isBorrowerExists) {
+      notFoundLogger(req);
       return res.status(404).json({
         success: false,
-        message: `There is no borrowed books in the database right now not`,
+        message: `Borrower with id ${body.BorrowerId} not found in the database`,
         data: {},
       });
     }
-    okLogger();
-    res.status(200).json({
+    const createdBook = await BorrowedBook.create(body);
+    createdLogger(req);
+    res.status(201).json({
       success: true,
-      message: 'Borrowed books fetched successfully',
-      data: borrowedBooks,
+      message: `Borrowed Book related to the borrower with id ${body.BorrowerId} created successfully`,
+      data: createdBook,
     });
   } catch (err) {
     next(err);
