fc58c65fde80a4233913752632debb9c79aeb8bf
04bdull4h <s201830040@kfupm.edu.sa>
2021-08-30 12:07:34 +0300

    implement crud ops. for employeeController

--- api/controllers/employeeController.js
+++ api/controllers/employeeController.js
@@ -1,4 +1,4 @@
-const {} = require('../models');
+const { Employee } = require('../models');
 const { validationResult } = require('express-validator');
 const {
   okLogger,
@@ -7,14 +7,108 @@ const {
   notFoundLogger,
 } = require('../utils/loggerMethods');
 
-const fetchAllEmployees = async (req, res, next) => {};
-const fetchEmployeeById = async (req, res, next) => {};
+const fetchAllEmployees = async (req, res, next) => {
+  try {
+    const employees = await Employee.findAll();
+    if (!employees) {
+      return res.status(404).json({
+        success: false,
+        message: `There is no employees in the database right now not`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: 'Employees fetched successfully',
+      data: employees,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const fetchEmployeeById = async (req, res, next) => {
+  try {
+    const employeeId = req.params.id;
+    const employee = await Employee.findByPk(employeeId);
+    if (!employee) {
+      notFoundLogger(req);
+      return res.status(404).json({
+        success: false,
+        message: `Employee with id ${employeeId} not found in the database`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: `Employee with id ${employeeId} fetched successfully`,
+      result: employee,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 const createEmployee = async (req, res, next) => {
   try {
-  } catch (err) {}
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      badRequestLogger(req);
+      return res.status(400).json({
+        success: false,
+        msg: 'Validation errors',
+        errors: errors.array(),
+      });
+    }
+    const body = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      jobTitle: req.body.jobTitle,
+      department: req.body.department,
+      salary: req.body.salary,
+      hireDate: req.body.hireDate,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+    };
+    const createdEmployee = await Employee.create(body);
+    createdLogger(req);
+    res.status(201).json({
+      success: true,
+      message: 'Employee created successfully',
+      data: createdEmployee,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 const updateEmployeeById = async (req, res, next) => {};
-const deleteEmployeeById = async (req, res, next) => {};
+
+const deleteEmployeeById = async (req, res, next) => {
+  try {
+    const employeeId = req.params.id;
+    const deletedEmployee = await Borrower.destroy({
+      where: { id: employeeId },
+    });
+    if (!deletedEmployee) {
+      notFoundLogger(req);
+      return res.status(404).json({
+        success: false,
+        message: `Employee with id ${employeeId} not found in the database`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: `Employee with id ${employeeId} deleted successfully`,
+      data: {},
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   fetchAllEmployees,
