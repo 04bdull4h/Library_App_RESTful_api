99421966dd0781516da33f6cda3a6ee7b447e8c6
04bdull4h <s201830040@kfupm.edu.sa>
2021-08-30 12:08:13 +0300

    create routes for employeeRouter

--- api/routes/employeeRouter.js
+++ api/routes/employeeRouter.js
@@ -5,10 +5,24 @@ const {
   accessTokenMiddleware,
 } = require('../middlewares/accessTokenMiddleware');
 const bodyValidatorMiddleware = require('../middlewares/bodyValidatorMiddleware');
-
+const employeeController = require('../controllers/employeeController');
 /**
  *@doc api/v1/borrowers/ endpoints
  *@controller borrowerController
  */
+
+router.get('/', employeeController.fetchAllEmployees);
+router.get('/:id', employeeController.fetchEmployeeById);
+router.post(
+  '/',
+  bodyValidatorMiddleware('createEmployee'),
+  employeeController.createEmployee
+);
+router.put(
+  '/:id',
+  bodyValidatorMiddleware('updateEmployeeById'),
+  employeeController.updateEmployeeById
+);
+router.delete('/:id', employeeController.deleteEmployeeById);
 
 module.exports = router;
