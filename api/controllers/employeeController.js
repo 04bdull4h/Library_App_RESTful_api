ae49e05275ae65d4467ebd4e21f2701e62ff4fb7
04bdull4h <s201830040@kfupm.edu.sa>
2021-08-30 11:28:10 +0300

    requiring some dep.

--- api/controllers/employeeController.js
+++ api/controllers/employeeController.js
@@ -1,6 +1,18 @@
+const {} = require('../models');
+const { validationResult } = require('express-validator');
+const {
+  okLogger,
+  createdLogger,
+  badRequestLogger,
+  notFoundLogger,
+} = require('../utils/loggerMethods');
+
 const fetchAllEmployees = async (req, res, next) => {};
 const fetchEmployeeById = async (req, res, next) => {};
-const createEmployee = async (req, res, next) => {};
+const createEmployee = async (req, res, next) => {
+  try {
+  } catch (err) {}
+};
 const updateEmployeeById = async (req, res, next) => {};
 const deleteEmployeeById = async (req, res, next) => {};
 
