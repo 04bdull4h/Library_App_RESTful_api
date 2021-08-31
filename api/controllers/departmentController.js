2a985a926cdc9b50a92ed3e9b1e7ad76857540bb
04bdull4h <s201830040@kfupm.edu.sa>
2021-08-31 15:31:08 +0300

    impelement fetchAllDepartments

--- api/controllers/departmentController.js
+++ api/controllers/departmentController.js
@@ -7,15 +7,50 @@ const {
   notFoundLogger,
 } = require('../utils/loggerMethods');
 
-const fetchAllDepartments = async (req, res, next) => {};
-const fetchDepartmentById = async (req, res, next) => {};
-const createDepartment = async (req, res, next) => {};
-const updateDepartmentById = async (req, res, next) => {};
-const deleteDepartmentById = async (req, res, next) => {};
+const fetchAllDepartments = async (req, res, next) => {
+  try {
+    const departments = await Department.findAll();
+    if (!departments) {
+      return res.status(404).json({
+        success: false,
+        message: `There is no departments in the database right now not`,
+        data: {},
+      });
+    }
+    okLogger(req);
+    res.status(200).json({
+      success: true,
+      message: 'Departments fetched successfully',
+      data: departments,
+    });
+  } catch (err) {}
+};
+
+const fetchAllEmployeesByDepartmentId = async (req, res, next) => {
+  try {
+  } catch (err) {}
+};
+const fetchDepartmentById = async (req, res, next) => {
+  try {
+  } catch (err) {}
+};
+const createDepartment = async (req, res, next) => {
+  try {
+  } catch (err) {}
+};
+const updateDepartmentById = async (req, res, next) => {
+  try {
+  } catch (err) {}
+};
+const deleteDepartmentById = async (req, res, next) => {
+  try {
+  } catch (err) {}
+};
 
 module.exports = {
   fetchAllDepartments,
   fetchDepartmentById,
+  fetchAllEmployeesByDepartmentId,
   createDepartment,
   updateDepartmentById,
   deleteDepartmentById,
